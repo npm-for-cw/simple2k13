17f458dd5e0259af2802def0d60421f85fd943a4
David2k13 <1096486917@qq.com>
2022-08-25 17:04:03 +0800

    perfect：canvasDrawImage

--- src/image/canvasDrawImage/index.ts
+++ src/image/canvasDrawImage/index.ts
@@ -1,14 +1,25 @@
 
-const canvasDrawImage = (canvas: HTMLCanvasElement, url: string, zoom: number = 4) => {
+const canvasDrawImage = (canvas: HTMLCanvasElement, url: string, zoom: number = 1) => {
   return new Promise((resolve, reject) => {
     try {
       const ctx = canvas?.getContext('2d')
+      const cW = canvas.width
+      const cH = canvas.height
+      let scale = 1
+
       let img = new Image()
       img.src = url
       img.onload = () => {
-        const imgW = img.width * zoom
-        const imgH = img.height * zoom
-        ctx?.drawImage(img, (canvas.width - imgW) / 2, (canvas.height - imgH) / 2, imgW, imgH)
+        let iW = img.width
+        let iH = img.height
+        if (cW <= iH) {
+          scale = cW / iW
+        } else {
+          scale = cH / iH
+        }
+        iW = iW * scale * zoom
+        iH = iH * scale * zoom
+        ctx?.drawImage(img, (cW - iW) / 2, (cH - iH) / 2, iW, iH)
         resolve('success')
       }
       img.onerror = (error) => {

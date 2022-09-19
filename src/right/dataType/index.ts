2b7393aa7cd36601f00819ae864aad413ae338b4
David2k13 <1096486917@qq.com>
2022-09-19 18:58:40 +0800

    perf: datatype lowercase

--- src/right/dataType/index.ts
+++ src/right/dataType/index.ts
@@ -1,8 +1,11 @@
 
-const dataType = (data: any):string => {
-  let result = (typeof data)
+const dataType = (data: any, options?: { lowercase: boolean }): string => {
+  const result = (typeof data)
+  const { lowercase = false } = options || {}
+
   if (result === 'object') {
-    return Object.prototype.toString.call(data).slice(8,-1)
+    const result = Object.prototype.toString.call(data).slice(8, -1)
+    return lowercase ? result.toLowerCase() : result
   }
   return result
 }

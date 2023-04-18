2bf8d24ccf17f03c91397c909db41572c9cb25a4
David2k13 <1096486917@qq.com>
2023-04-18 15:26:34 +0800

    fix: promiseQueue cancel

--- src/functions/PromiseQueue/index.ts
+++ src/functions/PromiseQueue/index.ts
@@ -12,18 +12,33 @@ class PromiseQueue {
   func: Prorps['func']
   limit: number
   count: number
+  pendingTask: Array<{
+    arr: Prorps['arr']
+    func: Prorps['func']
+    limit: number
+  }>
   finishSResolve: (value: unknown) => void
   constructor() {
     this.taskId = 0
     this.count = 0
     this.arr = []
     this.limit = 0
+    this.pendingTask = []
     this.func = () => ({})
     this.finishSResolve = () => ({})
   }
   init(props: Prorps) {
     const { arr = [], func, limit = 6 } = props || {}
     if (!arr.length || !func) return Promise.reject('arr and func is required')
+
+    if (this.arr.length) {
+      this.pendingTask.push({
+        arr: [...this.arr],
+        func: this.func,
+        limit: this.limit
+      })
+    }
+
     this.count = 0
     this.arr = [...arr]
     this.limit = Math.min(limit, arr.length)
@@ -35,8 +50,17 @@ class PromiseQueue {
   }
   async loop(taskId: number) {
     if (taskId !== this.taskId) return
-    if (!this.arr.length) {
-      (this.count <= 0) ? this.finishSResolve('finish') : setTimeout(() => this.loop(this.taskId), 100);
+    if (this.arr.length === 0) {
+      if (this.count <= 0) {
+        this.finishSResolve('finish')
+        const { arr, func, limit } = this.pendingTask.shift() || {}
+        if (arr?.length && func) {
+          this.taskId += 1
+          this.init({ arr, func, limit })
+        }
+      } else {
+        setTimeout(() => this.loop(this.taskId), 100)
+      }
       return
     }
     this.count += 1
@@ -52,6 +76,8 @@ class PromiseQueue {
   }
   cancel() {
     this.taskId += 1
+    this.arr.length = 0
+    this.pendingTask.length = 0
     return Promise.resolve('cancel')
   }
 

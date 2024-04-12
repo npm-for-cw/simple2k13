125f0dd0161b68584cf1cbd782dc85b1a0b13837
cw2k13 <1096486917@qq.com>
2024-04-12 15:30:42 +0800

    perf: IndexedDB dispatch trycath
    1.1.0

--- src/functions/IndexedDB/index.test.ts
+++ src/functions/IndexedDB/index.test.ts
@@ -1,28 +1,40 @@
-import { IndexedDB } from '../..'
+/** @format */
 
+import { IndexedDB } from "../..";
+
+const dbName = "testDB";
+const version = 1;
+const dispatchName = "appdispatch";
 let count: number;
 let res: any;
-describe('IndexedDB', () => {
-  test('IndexedDB', async () => {
-    const dbName = 'testDB';
-    const version = 1;
-    const dispatchName = 'appdispatch';
-    const db = new IndexedDB(dbName, dispatchName, version)
-    await db.open()
-    await db.dispatch('put', { more: true }, 1, 'one')
-    await db.dispatch('put', { more: false }, 2, 'two')
-    await db.dispatch('put', undefined, 3, 'three')
-    res = await db.dispatch('get', { more: false }, 'one')
-    expect(res).toBe(1)
-    res = await db.dispatch('get', { more: true }, 'two')
-    expect(res.result).toBe(2)
-    count = await db.dispatch('count', undefined) as number
-    expect(count).toBe(3)
-    await db.dispatch('delete', undefined, 'one')
-    count = await db.dispatch('count', undefined) as number
-    expect(count).toBe(2)
-    await db.dispatch('clear', undefined)
-    count = await db.dispatch('count', undefined) as number
-    expect(count).toBe(0)
-  })
-})
+
+describe("IndexedDB", () => {
+  test("IndexedDB", async () => {
+    const db = new IndexedDB(dbName, dispatchName, version);
+    await db.open();
+    await db.dispatch("put", { more: true }, 1, "one");
+    await db.dispatch("put", { more: false }, 2, "two");
+    await db.dispatch("put", undefined, 3, "three");
+    res = await db.dispatch("get", { more: false }, "one");
+    expect(res).toBe(1);
+    res = await db.dispatch("get", { more: true }, "two");
+    expect(res.result).toBe(2);
+    count = (await db.dispatch("count", undefined)) as number;
+    expect(count).toBe(3);
+    await db.dispatch("delete", undefined, "one");
+    count = (await db.dispatch("count", undefined)) as number;
+    expect(count).toBe(2);
+    await db.dispatch("clear", undefined);
+    count = (await db.dispatch("count", undefined)) as number;
+    expect(count).toBe(0);
+  });
+  test("IndexedDB No database connection", async () => {
+    const db = new IndexedDB(dbName, dispatchName, version);
+    try {
+      await db.dispatch("put", { more: true }, 1, "one");
+    } catch (error) {
+      console.log(error, "errorIN");
+      expect(error.message).toBeInstanceOf(Error);
+    }
+  });
+});

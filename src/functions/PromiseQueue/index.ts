77bb2b733159a85928984aa73a63db3557321d17
cw <1096486917@qq.com>
2025-01-02 17:33:55 +0800

    cil: ts PromiseQueue

--- src/functions/PromiseQueue/index.ts
+++ src/functions/PromiseQueue/index.ts
@@ -1,87 +1,81 @@
-
-type Func = (...params: unknown[]) => unknown;
-interface Prorps {
-  arr: unknown[]
-  func: Func
-  limit?: number
+/** @format */
+type Func<T> = (params: T) => void;
+interface Prorps<T> {
+  arr: T[];
+  func: Func<T>;
+  limit?: number;
 }
 
-class PromiseQueue {
-  taskId: number
-  arr: Prorps['arr']
-  func: Prorps['func']
-  limit: number
-  count: number
-  pendingTask: Array<{
-    arr: Prorps['arr']
-    func: Prorps['func']
-    limit: number
-  }>
-  finishSResolve: (value: unknown) => void
+class PromiseQueue<T> {
+  taskId: number;
+  arr: Prorps<T>["arr"];
+  func: Prorps<T>["func"];
+  limit: number;
+  count: number;
+  pendingTask: Array<Prorps<T>>;
+  finishSResolve: (value: unknown) => void;
   constructor() {
-    this.taskId = 0
-    this.count = 0
-    this.arr = []
-    this.limit = 0
-    this.pendingTask = []
-    this.func = () => ({})
-    this.finishSResolve = () => ({})
+    this.taskId = 0;
+    this.count = 0;
+    this.arr = [];
+    this.limit = 0;
+    this.pendingTask = [];
+    this.func = () => ({});
+    this.finishSResolve = () => ({});
   }
-  init(props: Prorps) {
-    const { arr = [], func, limit = 6 } = props || {}
-    if (!arr.length || !func) return Promise.reject('arr and func is required')
+  init(props: Prorps<T>) {
+    const { arr = [], func, limit = 6 } = props || {};
+    if (!arr.length || !func) return Promise.reject("arr and func is required");
 
     if (this.arr.length) {
       this.pendingTask.push({
         arr: [...this.arr],
         func: this.func,
-        limit: this.limit
-      })
+        limit: this.limit,
+      });
     }
 
-    this.count = 0
-    this.arr = [...arr]
-    this.limit = Math.min(limit, arr.length)
-    this.func = func
-    this.loop(this.taskId)
+    this.count = 0;
+    this.arr = [...arr];
+    this.limit = Math.min(limit, arr.length);
+    this.func = func;
+    this.loop(this.taskId);
     return new Promise((resolve) => {
-      this.finishSResolve = resolve
-    })
+      this.finishSResolve = resolve;
+    });
   }
   async loop(taskId: number) {
-    if (taskId !== this.taskId) return
+    if (taskId !== this.taskId) return;
     if (this.arr.length === 0) {
       if (this.count <= 0) {
-        this.finishSResolve('finish')
-        const { arr, func, limit } = this.pendingTask.shift() || {}
+        this.finishSResolve("finish");
+        const { arr, func, limit } = this.pendingTask.shift() || {};
         if (arr?.length && func) {
-          this.taskId += 1
-          this.init({ arr, func, limit })
+          this.taskId += 1;
+          this.init({ arr, func, limit });
         }
       } else {
-        setTimeout(() => this.loop(this.taskId), 100)
+        setTimeout(() => this.loop(this.taskId), 100);
       }
-      return
+      return;
     }
-    this.count += 1
+    this.count += 1;
     if (this.count < this.limit) {
-      this.loop(this.taskId)
+      this.loop(this.taskId);
     }
-    await this.func(this.arr.shift())
-    if (taskId !== this.taskId) return
+    await this.func(this.arr.shift() as T);
+    if (taskId !== this.taskId) return;
     if (this.count >= this.limit) {
-      this.loop(this.taskId)
+      this.loop(this.taskId);
     }
-    this.count -= 1
+    this.count -= 1;
   }
   cancel() {
-    this.taskId += 1
-    this.arr.length = 0
-    this.pendingTask.length = 0
-    return Promise.resolve('cancel')
+    this.taskId += 1;
+    this.arr.length = 0;
+    this.pendingTask.length = 0;
+    return Promise.resolve("cancel");
   }
-
 }
 
-export default PromiseQueue
-
+export default PromiseQueue;
